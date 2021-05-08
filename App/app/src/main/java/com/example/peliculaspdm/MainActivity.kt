a6df75df569503ba90bc82e749c10762ddc69d98
Francisco Beltrán <currob1999@gmail.com>
2021-05-08 20:30:06 +0200

    Mejorando sistema de recomendación

--- App/app/src/main/java/com/example/peliculaspdm/MainActivity.kt
+++ App/app/src/main/java/com/example/peliculaspdm/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.peliculaspdm
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.hardware.Sensor
@@ -7,9 +8,12 @@ import android.hardware.Sensor.TYPE_ACCELEROMETER
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.squareup.picasso.Picasso
 import com.synnapps.carouselview.CarouselView
@@ -20,7 +24,6 @@ import org.json.JSONObject
 import java.io.IOException
 import kotlin.math.abs
 import kotlin.random.Random
-
 
 class MainActivity : AppCompatActivity(), SensorEventListener {
     var idsesion: String = ""
@@ -37,6 +40,8 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
             JSONObject(),
             JSONObject(),
             JSONObject())
+    var ultimaPeliAleatoria = -1
+    var nombrePeliRecomendada = ""
 
     //Variables para el acelerómetro
     var sensorManager: SensorManager? = null
@@ -86,12 +91,14 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         Picasso.with(this).load(enlaceImagen).into(imageView)
     }
 
+    @SuppressLint("SetTextI18n")
+    @RequiresApi(Build.VERSION_CODES.KITKAT)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        sensor = sensorManager!!.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        sensor = sensorManager!!.getDefaultSensor(TYPE_ACCELEROMETER)
         sensorManager!!.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL)
 
         requestToken = intent.getStringExtra("RequestToken")!!
@@ -122,6 +129,14 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         val carouselView = findViewById<CarouselView>(R.id.carouselView)
         carouselView.pageCount = vectorPelisRecomendadasDefinitivas.size
         carouselView.setImageListener(imageListener)
+
+        val vectorPelisFavoritas = JSONArray(pelisFavoritas)
+
+        if(vectorPelisFavoritas.length() > 19){
+            var sitegustoView = findViewById<TextView>(R.id.sitegusto)
+            sitegustoView.text = "Si te gusto $nombrePeliRecomendada te gustará..."
+            sitegustoView.textSize = 18F
+        }
     }
 
     fun noAsistente(view: View){
@@ -203,42 +218,71 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
             })
     }
 
+    @RequiresApi(Build.VERSION_CODES.KITKAT)
     fun peticionRecomendaciones(){
-        val idCuenta = JSONObject(infousuario).getInt("id")
-        Log.i("IDCUENTA", idCuenta.toString())
-        val request = Request.Builder()
-            .url("https://api.themoviedb.org/3/account/$idCuenta/favorite/movies?api_key=ecfe4f06a0f028c3618838df92bfea77&session_id=$idsesion")
-            .build()
-        val cliente = OkHttpClient()
-
-        cliente.newCall(request)
-            .enqueue(object : Callback {
-                override fun onFailure(call: Call?, e: IOException?) {
-                    // Error
-                    runOnUiThread {
-                        // For the example, you can show an error dialog or a toast
-                        // on the main UI thread
-                    }
-                }
-
-                @Throws(IOException::class)
-                override fun onResponse(call: Call?, response: Response) {
-                    val res = JSONObject(response.body()!!.string())
-
-                    pelisFavoritas = res.getJSONArray("results").toString()
-                }
-            })
+        peticionPeliculasFavoritas()
 
         while(pelisFavoritas == ""){
 
         }
+        val arrayPelis = JSONArray(pelisFavoritas)
 
+        if(arrayPelis.length() < 20){
+            pelisFavoritas = ""
+            peticionTendenciasPeliculas()
+            while(pelisFavoritas == ""){
+
+            }
+            val mensaje = findViewById<TextView>(R.id.mensajePeliculasFavoritas)
+            mensaje.text = "Valore positivamente 20 películas para obtener recomendaciones personalizadas"
+        }
+
+        peticionRecomendacionConPelicula()
+
+        while(pelisRecomendadas == ""){
+
+        }
+
+        eliminaFavoritosRecomendaciones()
+    }
+
+    fun peticionPeliculasFavoritas(){
+        val idCuenta = JSONObject(infousuario).getInt("id")
+        Log.i("IDCUENTA", idCuenta.toString())
+        val request = Request.Builder()
+                .url("https://api.themoviedb.org/3/account/$idCuenta/favorite/movies?api_key=ecfe4f06a0f028c3618838df92bfea77&session_id=$idsesion")
+                .build()
+        val cliente = OkHttpClient()
+
+        cliente.newCall(request)
+                .enqueue(object : Callback {
+                    override fun onFailure(call: Call?, e: IOException?) {
+                        // Error
+                        runOnUiThread {
+                            // For the example, you can show an error dialog or a toast
+                            // on the main UI thread
+                        }
+                    }
+
+                    @Throws(IOException::class)
+                    override fun onResponse(call: Call?, response: Response) {
+                        val res = JSONObject(response.body()!!.string())
+
+                        pelisFavoritas = res.getJSONArray("results").toString()
+                    }
+                })
+    }
+
+    fun peticionRecomendacionConPelicula(){
         val jsonArrayFavoritas = JSONArray(pelisFavoritas)
         val random = Random(System.currentTimeMillis())
-        val peliAleatoria = random.nextInt(0, jsonArrayFavoritas.length())
+        val cliente = OkHttpClient()
 
-        val peliElegida = jsonArrayFavoritas.getJSONObject(peliAleatoria)
+        ultimaPeliAleatoria = random.nextInt(0, jsonArrayFavoritas.length())
+
+        val peliElegida = jsonArrayFavoritas.getJSONObject(ultimaPeliAleatoria)
         val idPeli = peliElegida.getInt("id")
+        nombrePeliRecomendada = peliElegida.getString("title")
 
         val request2 = Request.Builder()
                 .url("https://api.themoviedb.org/3/movie/$idPeli/recommendations?api_key=ecfe4f06a0f028c3618838df92bfea77")
@@ -259,6 +303,53 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
                         val res = JSONObject(response.body()!!.string())
 
                         pelisRecomendadas = res.getJSONArray("results").toString()
+                    }
+                })
+    }
+
+    @RequiresApi(Build.VERSION_CODES.KITKAT)
+    fun eliminaFavoritosRecomendaciones(){
+        var arrayPelisRecomendadas = JSONArray(pelisRecomendadas)
+        var arrayPelisFavoritas = JSONArray(pelisFavoritas)
+
+        for(i in 0 until arrayPelisFavoritas.length()){
+            var peliculaFavorita = arrayPelisFavoritas.getJSONObject(i)
+
+            for(j in 0 until arrayPelisRecomendadas.length()){
+                if(j<arrayPelisRecomendadas.length()){
+                    var peliculaRecomendada = arrayPelisRecomendadas.getJSONObject(j)
+
+                    if (peliculaFavorita.getString("title") == peliculaRecomendada.getString("title")){
+                        arrayPelisRecomendadas.remove(j)
+                    }
+                }
+            }
+        }
+
+        pelisRecomendadas = arrayPelisRecomendadas.toString()
+    }
+
+    fun peticionTendenciasPeliculas(){
+        val request = Request.Builder()
+                .url("https://api.themoviedb.org/3/trending/movie/week?api_key=ecfe4f06a0f028c3618838df92bfea77")
+                .build()
+        val cliente = OkHttpClient()
+
+        cliente.newCall(request)
+                .enqueue(object : Callback {
+                    override fun onFailure(call: Call?, e: IOException?) {
+                        // Error
+                        runOnUiThread {
+                            // For the example, you can show an error dialog or a toast
+                            // on the main UI thread
+                        }
+                    }
+
+                    @Throws(IOException::class)
+                    override fun onResponse(call: Call?, response: Response) {
+                        val res = JSONObject(response.body()!!.string())
+
+                        pelisFavoritas = res.getJSONArray("results").toString()
                     }
                 })
     }
@@ -319,6 +410,7 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
                 })
     }
 
+    @RequiresApi(Build.VERSION_CODES.KITKAT)
     override fun onSensorChanged(event: SensorEvent?) {
         if(event!!.sensor.type == TYPE_ACCELEROMETER){
             val x = event.values[0]
@@ -334,7 +426,7 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
                 val speed = abs(x + y + z - last_x - last_y - last_z) / diffTime * 10000
 
                 if (speed > SHAKE_THRESHOLD) {
-                    Log.i("MOVIMIENTO", "SI $speed")
+                    cambiarPeliculasCarrusel()
                 }
 
                 last_x = x
@@ -358,4 +450,27 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         sensorManager!!.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL)
     }
 
+    @RequiresApi(Build.VERSION_CODES.KITKAT)
+    fun cambiarPeliculasCarrusel(){
+        peticionRecomendaciones()
+        while (pelisRecomendadas == ""){
+            //Pequeña espera para que el vector no sea vacío
+        }
+
+        var vectorPelisRecomendadas = JSONArray(pelisRecomendadas)
+
+        for(i in 0 until 4){
+            vectorPelisRecomendadasDefinitivas[i] = vectorPelisRecomendadas.getJSONObject(i)
+        }
+        val carouselView = findViewById<CarouselView>(R.id.carouselView)
+        carouselView.pageCount = vectorPelisRecomendadasDefinitivas.size
+        carouselView.setImageListener(imageListener)
+
+        val vectorPelisFavoritas = JSONArray(pelisFavoritas)
+        if(vectorPelisFavoritas.length() > 19){
+            var sitegustoView = findViewById<TextView>(R.id.sitegusto)
+            sitegustoView.text = "Si te gusto $nombrePeliRecomendada te gustará..."
+            sitegustoView.textSize = 18F
+        }
+    }
 }
